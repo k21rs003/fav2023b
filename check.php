3ece71485d38b22a46383c3a5c1e546051f56928
k21rs003 <135787746+k21rs003@users.noreply.github.com>
2023-07-13 22:50:37 +0900

    Update check.php

--- check.php
+++ check.php
@@ -1,66 +1,7 @@
 <?php
 
-function getWeekday($date)
-{
-    $wday = date('w', strtotime($date));
-    $wdays = ['日', '月', '火', '水', '木', '金', '土'];
-    return $wdays[$wday];
-}
-
-$coreTimes = [
-    1 => '9:00~11:50',
-    2 => '12:00~14:50',
-    3 => '15:00~16:50'
-];
-
-$spHoliday = [
-    '01-01' => '元日',
-    '01-02' => '振替休日',
-    '01-09' => '成人の日',
-    '02-11' => '建国記念の日',
-    '02-23' => '天皇誕生日',
-    '03-21' => '春分の日',
-    '04-29' => '昭和の日',
-    '05-03' => '憲法記念日',
-    '05-04' => 'みどりの日',
-    '05-05' => 'こどもの日',
-    '07-17' => '海の日',
-    '08-11' => '山の日',
-    '09-18' => '敬老の日',
-    '09-23' => '秋分の日',
-    '10-09' => 'スポーツの日',
-    '11-03' => '文化の日',
-    '11-23' => '勤労感謝の日'
-];
-
-$facility = [ 
-    '01-05' => ['name' => '会議室', 'coreTime' => [1]],
-    '02-10' => ['name' => '会議室', 'coreTime' => [2]],
-    '03-05' => ['name' => '面接室A', 'coreTime' => [1]],
-    '04-05' => ['name' => '会議室', 'coreTime' => [1]],
-    '05-10' => ['name' => '面接室A', 'coreTime' => [2]],
-    '06-05' => ['name' => '会議室', 'coreTime' => [1]],
-    '07-10' => ['name' => '会議室', 'coreTime' => [1]],
-    '08-01' => ['name' => '面接室B', 'coreTime' => [1, 2]],
-    '08-07' => ['name' => '面接室B', 'coreTime' => [1, 2, 3]],
-    '09-05' => ['name' => '会議室', 'coreTime' => [2, 3]],
-    '10-10' => ['name' => '面接室B', 'coreTime' => [3]],
-    '11-05' => ['name' => '会議室', 'coreTime' => [1]],
-    '12-05' => ['name' => '会議室', 'coreTime' => [1]],
-    '01-07' => ['name' => '面接室A', 'coreTime' => [2]],
-    '02-07' => ['name' => '会議室', 'coreTime' => [1]],
-    '02-08' => ['name' => '面接室A', 'coreTime' => [2, 3]],
-    '03-08' => ['name' => '会議室', 'coreTime' => [1]],
-    '04-08' => ['name' => '会議室', 'coreTime' => [2]],
-    '05-09' => ['name' => '面接室B', 'coreTime' => [3]],
-    '06-09' => ['name' => '面接室A', 'coreTime' => [2]],
-    '07-09' => ['name' => '会議室', 'coreTime' => [1, 2]],
-    '08-09' => ['name' => '会議室', 'coreTime' => [1]],
-    '08-10' => ['name' => '面接室A', 'coreTime' => [2]],
-    '09-10' => ['name' => '面接室B', 'coreTime' => [3]],
-    '10-15' => ['name' => '会議室', 'coreTime' => [1]],
-];
-
+include ('lib/Calendar.php');
+include ('lib/Input.php');
 
 if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
     $y = $_POST['year'];
@@ -83,11 +24,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
     ];
     $facilityName = ($f >= 1 && $f <= 3) ? $facilities[$f] : '-';
 
-    $holidayWdays= [0, 6]; // 土日の配列
-
-    //$r = $reservedDates[$facilityName] ?? [];
-
-    //if(isset($r[]))
+    $holidayWdays = [0, 6]; // 土日の配列
 
     if ($d == 0) {
         echo "検索: {$y}年{$m}月<br>";
@@ -104,7 +41,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
             $formattedDate = sprintf("%04d-%02d-%02d", $y, $m, $day);
             $dateKey = sprintf("%02d-%02d", $m, $day);
             $holidayName = $spHoliday[$dateKey] ?? '';
-            $reserveData = $facility[$dateKey] ?? ''; //
+            $reserveData = $facility[$dateKey] ?? '';
 
             $dayCount = sprintf("%02d", $day);
             echo "{$dayCount} ({$wdays[$currentDayWeek]}):";
@@ -120,9 +57,9 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
             } else {
                 if (isset($facility[$dateKey]) && $facility[$dateKey]['name'] === $facilityName) {
                     $coreTime = $facility[$dateKey]['coreTime'];
-                
+
                     //echo "　-core time:";
-                
+
                     if (is_array($coreTime)) {
                         foreach ($coreTime as $time) {
                             echo " <span style='color: blue;'>{$coreTimes[$time]}</span>";
@@ -130,7 +67,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
                     } else {
                         echo " <span style='color: blue;'>{$coreTimes[$coreTime]}</span>";
                     }
-                
+
                     echo "<br>";
                 } elseif ($facilityName === '-') {
                     if (!empty($reserveData)) {
@@ -138,7 +75,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
                         $coreTime = $reserveData['coreTime'];
                         echo "<br>　*Reserve facility: <span style='color: blue;'>{$reserveName}</span><br>";
                         echo "　-core time: ";
-                
+
                         if (is_array($coreTime)) {
                             foreach ($coreTime as $time) {
                                 echo " <span style='color: blue;'>{$coreTimes[$time]}</span>";
@@ -146,7 +83,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
                         } else {
                             echo " <span style='color: blue;'>{$coreTimes[$coreTime]}</span>";
                         }
-                
+
                         echo "<br>";
                     } else {
                         echo "<br>";
@@ -164,7 +101,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
         $currentDayWeek = date('w', mktime(0, 0, 0, $m, $d, $y));
         $dateKey = sprintf("%02d-%02d", $m, $d);
         $holidayName = $spHoliday[$dateKey] ?? '';
-        $reserveData = $facility[$dateKey] ?? ''; //
+        $reserveData = $facility[$dateKey] ?? '';
 
         if (in_array($holidayName, $spHoliday)) {
             echo "-name: {$holidayName}<br>";
@@ -175,9 +112,9 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
         } else {
             if (isset($facility[$dateKey]) && $facility[$dateKey]['name'] === $facilityName) {
                 $coreTime = $facility[$dateKey]['coreTime'];
-            
+
                 echo "core time:";
-            
+
                 if (is_array($coreTime)) {
                     foreach ($coreTime as $time) {
                         echo "{$coreTimes[$time]}　";
@@ -185,7 +122,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
                 } else {
                     echo "{$coreTimes[$coreTime]}　";
                 }
-            
+
                 echo "<br>";
             } elseif ($facilityName === '-') {
                 if (!empty($reserveData)) {
@@ -193,7 +130,7 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
                     $coreTime = $reserveData['coreTime'];
                     echo "<br>　*Reserve facility: <span style='color: blue;'>{$reserveName}</span><br>";
                     echo "　-core time: ";
-            
+
                     if (is_array($coreTime)) {
                         foreach ($coreTime as $time) {
                             echo " <span style='color: blue;'>{$coreTimes[$time]}</span>";
@@ -201,12 +138,12 @@ if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
                     } else {
                         echo " <span style='color: blue;'>{$coreTimes[$coreTime]}</span>";
                     }
-            
+
                     echo "<br>";
                 } else {
                     echo "[予約なし]";
                 }
-            }else{
+            } else {
                 echo "[予約なし]";
             }
         }
